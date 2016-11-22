b45eb2a10fc59c02de335691c7575ea6841c617e
dbenitez <dbenitez@107-14>
2016-11-22 11:12:04 +0100

    seghszdtghsrth

---
+++ FormularioBinding/FormularioBinding/Views/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using FormularioBinding.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace FormularioBinding
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.ViewModel = (clsMainPageVM) this.DataContext;
+        }
+        public clsMainPageVM ViewModel { get; }
+
+
+        private void btnGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            this.txtNombre.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.txtApellidos.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.txtFechaNac.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.txtTelefono.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+            this.txtDireccion.GetBindingExpression(TextBox.TextProperty).UpdateSource();
+        }
+    }
+}
